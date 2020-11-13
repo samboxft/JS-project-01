6e2e3599593bf0e0affcaa47721bc5501767b168
ron <rinbar70@gmail.com>
2020-11-13 19:17:59 +0200

    Rearrange the code in displayAllTasks().

--- main.js
+++ main.js
@@ -33,6 +33,7 @@ function deleteTaskFromLocalStorage(i) {
 }
 
 function saveTask() {
+    
     // Take todoM objects:
     const todoBox = document.getElementById("todoBox");
     const dateBox = document.getElementById("dateBox");
@@ -62,46 +63,47 @@ function displayAllTasks() {
     // Get container todoM object: 
     const container = document.getElementById("container");
 
-    //clear container
+    // clear container
     container.innerHTML = "";
 
     let allTasks = getAllTasksFromLocalStorage();
 
-    //display 
+    // display 
     for (let i = 0; i < allTasks.length; i++) {
 
-        //  create container div: 
+        // create container div: 
         const note = document.createElement("div");
         note.classList.add("notes", "col-xs-3");
-        note.setAttribute("style", "min-width: 200px; min-height: 250px;")
-        container.appendChild(note);
 
         const top = document.createElement("div");
         top.classList.add("top");
-        note.appendChild(top)
 
-        //delete button, set attributes:{ "type", "class bootstrap", "onclickDeleteB" } add del event 
+        // delete button, set attributes:{ "type", "class bootstrap", "onclickDeleteB" } add del event 
         const deleteB = document.createElement("button");
         deleteB.classList.add("deleteB", "btn", "btn-info", "btn-sm");
         deleteB.setAttribute("type", "button");
         deleteB.setAttribute("onclick", `deleteB(${i})`);
-        top.appendChild(deleteB);
 
-        //span for glyph icon
+        // span for glyph icon
         const span = document.createElement("span");
         span.classList.add("glyphicon", "glyphicon-remove");
-        deleteB.appendChild(span);
 
-        //display note
+        // display note
         const p = document.createElement("p");
         p.innerHTML = allTasks[i].todo;
-        note.appendChild(p)
 
         // Div for date and time
         const bottom = document.createElement("div");
         bottom.classList.add("dateNTime");
         bottom.innerHTML = `Finish by: ${allTasks[i].date}<br>To be exact: ${allTasks[i].time}`;
+
+        // Connect all the elements to each other and add them to the document.
+        deleteB.appendChild(span);
+        top.appendChild(deleteB);
+        note.appendChild(top)
+        note.appendChild(p)
         note.appendChild(bottom);
+        container.appendChild(note);
     }
 }
 
